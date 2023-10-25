cab8e3d049a05f590030c639d52eb6bc6cb682e2
Tania Reyes Amador <taniareyesamador25@gmail.com>
2023-10-25 13:27:23 -0600

    Pruebas unitarias y de integración

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -5,16 +5,17 @@ sys.path.append("../")
 
 import pytest
 
+
 from src.calc.evaluar_expresion.evaluar_expresion import evaluar_expresion
 
 
-def obtener_datos_test_integracion():
+def obtener_datos_test_evaluar_expresion():
     return [
         ("(5+5)*(1.25-0.75)", 5),
         ("(8+7/5)*(15-3/8)", 137.475),
     ]
 
 
-@pytest.mark.parametrize("operacion, esperado", obtener_datos_test_integracion())
-def test_divide_parametrize(operacion, esperado):
+@pytest.mark.parametrize("operacion, esperado", obtener_datos_test_evaluar_expresion())
+def test_evaluar_expresion_parametrize(operacion, esperado):
     assert (evaluar_expresion(operacion) == esperado)
